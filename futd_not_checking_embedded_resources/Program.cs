3f300e6e5723b6e025fc433cee5d74e2652734b9
Osvaldo Calles <oscalles@microsoft.com>
2020-04-27 17:23:30 -0700

    Embedded Resources not checked by FUTD

---
+++ futd_not_checking_embedded_resources/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace futd_not_checking_embedded_resources
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
